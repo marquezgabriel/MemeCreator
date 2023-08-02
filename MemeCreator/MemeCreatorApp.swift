95cd78ec5efda7445bca582297008c9c5ee92357
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-08-02 09:31:31 -0400

    Initial Commit

---
+++ MemeCreator/MemeCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCreatorApp.swift
+//  MemeCreator
+//
+//  Created by Gabriel Marquez on 2023-08-02.
+//
+
+import SwiftUI
+
+@main
+struct MemeCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
